85fccfbd5ff1e40cdac3085d482e08a1b0bad328
Kelv Gooding <82043281+kelvgooding@users.noreply.github.com>
2023-10-14 19:05:53 +0100

    1.3
    - os and platform modules added. Used within an If/Else statement to confirm which OS is being used. This will now determine the path/filename to use.
    - Variables have been added for both Filename and Username.

--- imp_exp.py
+++ imp_exp.py
@@ -1,8 +1,8 @@
 """
 Author: Kelvin Gooding
 Created: 2023-08-08
-Updated: 2023-08-08
-Version: 1.0
+Updated: 2023-10-14
+Version: 1.1
 """
 
 #!/usr/bin/env python3
@@ -12,10 +12,15 @@ Version: 1.0
 import mysql.connector
 import csv
 from datetime import datetime
-
-# Custom Modules
-
+import os
+import platform
 import auth
+
+# Variables
+
+username = os.environ.get('USER')
+exp_filename = f'contacts_export_{datetime.today().strftime("%Y%m%d_%H%M%S")}.csv'
+imp_filename = 'contacts_import.csv'
 
 # MySQL Variables
 
@@ -40,7 +45,11 @@ def export_data():
     c.execute('SELECT * FROM contacts ORDER BY first_name ASC;')
     contacts_data = c.fetchall()
 
-    file = open(f'export/contacts_export_{datetime.today().strftime("%Y%m%d_%H%M%S")}.csv', 'w', newline="")
+    if platform.system() == 'Windows':
+        file = open(f'export/{exp_filename}', 'w', newline="")
+    elif platform.system() == 'Linux':
+        file = open(f'/home/{username}/apps/contacts/export/{exp_filename}', 'w', newline="")
+
     writer = csv.writer(file)
     writer.writerow(headings)
     writer.writerows(contacts_data)
@@ -49,13 +58,20 @@ def export_data():
 def import_data():
 
     # Select all data from the contacts table.
-    
-    with open('import/contacts_import.csv') as file:
 
-        reader_obj = csv.reader(file)
-
-        next(reader_obj)
-
-        for row in reader_obj:
-            c.execute(f'INSERT INTO contacts VALUES ("{row[0]}", "{row[1]}", "{row[2]}", "{row[3]}", "{row[4]}", "{row[5]}", "{row[6]}", "{row[7]}", "{row[8]}", "{row[9]}", CURRENT_TIMESTAMP)')
-            conn.commit()
+    if platform.system() == 'Windows':
+        with open(f'import\{imp_filename}') as file:
+            reader_obj = csv.reader(file)
+            next(reader_obj)
+            for row in reader_obj:
+                c.execute(f'INSERT INTO contacts VALUES ("{row[0]}", "{row[1]}", "{row[2]}", "{row[3]}", "{row[4]}", "{row[5]}", "{row[6]}", "{row[7]}", "{row[8]}", "{row[9]}", CURRENT_TIMESTAMP)')
+                conn.commit()
+        os.remove(f'import\{imp_filename}')
+    elif platform.system() == 'Linux':
+        with open(f'/home/{username}/apps/contacts/import/{imp_filename}') as file:
+            reader_obj = csv.reader(file)
+            next(reader_obj)
+            for row in reader_obj:
+                c.execute(f'INSERT INTO contacts VALUES ("{row[0]}", "{row[1]}", "{row[2]}", "{row[3]}", "{row[4]}", "{row[5]}", "{row[6]}", "{row[7]}", "{row[8]}", "{row[9]}", CURRENT_TIMESTAMP)')
+                conn.commit()
+        os.remove(f'/home/{username}/apps/contacts/import/{imp_filename}')
