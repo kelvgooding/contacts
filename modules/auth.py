b38fe2a6e228189ed3e22d9896b3e50cd2a8b928
Kelv Gooding <82043281+kelvgooding@users.noreply.github.com>
2023-10-20 22:25:56 +0100

    Add files via upload

--- modules/auth.py
+++ modules/auth.py
@@ -2,7 +2,19 @@ from configparser import ConfigParser
 import os
 
 config = ConfigParser()
-config.read(f'/home/{os.environ.get("USER")}/.config.ini')
+config.read(f'/home/pi/.config.ini')
+
+credentials_auth = {
+    'username' : config.get('CREDENTIALS', 'CREDENTIALS_USERNAME'),
+    'password' : config.get('CREDENTIALS', 'CREDENTIALS_PASSWORD'),
+    }
+
+smtp_auth = {
+    'server' : config.get('SMTP', 'SMTP_SERVER'),
+    'port' : config.get('SMTP', 'SMTP_PORT'),
+    'email' : config.get('SMTP', 'SMTP_EMAIL'),
+    'password' : config.get('SMTP', 'SMTP_PASSWORD'),
+    }
 
 mysql_db_auth = {
     'user' : config.get('MYSQL', 'MYSQL_DB_USERNAME'),
