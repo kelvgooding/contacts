d40a92774d8fa76bbe3c7bd229f1269e440ba05c
Kelv Gooding <82043281+kelvgooding@users.noreply.github.com>
2023-10-20 21:03:46 +0100

    Update auth.py

--- modules/auth.py
+++ modules/auth.py
@@ -4,18 +4,6 @@ import os
 config = ConfigParser()
 config.read(f'/home/{os.environ.get("USER")}/.config.ini')
 
-credentials_auth = {
-    'username' : config.get('CREDENTIALS', 'CREDENTIALS_USERNAME'),
-    'password' : config.get('CREDENTIALS', 'CREDENTIALS_PASSWORD'),
-    }
-
-smtp_auth = {
-    'server' : config.get('SMTP', 'SMTP_SERVER'),
-    'port' : config.get('SMTP', 'SMTP_PORT'),
-    'email' : config.get('SMTP', 'SMTP_EMAIL'),
-    'password' : config.get('SMTP', 'SMTP_PASSWORD'),
-    }
-
 mysql_db_auth = {
     'user' : config.get('MYSQL', 'MYSQL_DB_USERNAME'),
     'password' : config.get('MYSQL', 'MYSQL_DB_PASSWORD'),
